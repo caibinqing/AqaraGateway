37b375eb5c46140b46582c8e801e4650f8cc5773
Necroneco <caibinqing@live.com>
2023-12-23 14:34:31 +0800

    cover considered closed when position < 5

--- custom_components/aqara_gateway/cover.py
+++ custom_components/aqara_gateway/cover.py
@@ -117,7 +117,7 @@ class XiaomiGenericCover(GatewayGenericDevice, CoverEntity, RestoreEntity):
         """Return if the cover is closed."""
         if (position := self.current_cover_position) is None:
             return None
-        return position == 0
+        return position < 5
 
     def update(self, data: dict = None):
         """ update state """
