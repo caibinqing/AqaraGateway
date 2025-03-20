be267efcfb6338672a814f6f77212dc40a48f35d
Necroneco <caibinqing@live.com>
2025-03-20 22:04:00 +0800

    refactor: switch.py

--- custom_components/aqara_gateway/switch.py
+++ custom_components/aqara_gateway/switch.py
@@ -1,65 +1,36 @@
-"""Support for Aqara Switchs."""
+"""Support for Aqara Switches."""
+from typing import Any
 
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.const import ATTR_VOLTAGE, STATE_OFF, STATE_ON
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import STATE_OFF, STATE_ON
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.restore_state import RestoreEntity
 
 from . import DOMAIN, GatewayGenericDevice
-from .core.const import (
-    ATTR_CHIP_TEMPERATURE,
-    ATTR_FW_VER,
-    ATTR_IN_USE,
-    ATTR_LOAD_POWER,
-    ATTR_LQI,
-    ATTR_POWER_CONSUMED,
-    CHIP_TEMPERATURE,
-    ENERGY_CONSUMED,
-    FW_VER,
-    IN_USE,
-    LOAD_POWER,
-    LOAD_VOLTAGE,
-    LQI,
-    SWITCH_ATTRIBUTES,
-)
 from .core.gateway import Gateway
-from .core.utils import Utils
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     """ Perform the setup for Xiaomi/Aqara devices. """
     def setup(gateway: Gateway, device: dict, attr: str):
-        feature = Utils.get_feature_suppported(device["model"])
-        async_add_entities([
-            GatewaySwitch(gateway, device, attr, feature)
-        ])
+        async_add_entities([GatewaySwitch(gateway, device, attr)])
     aqara_gateway: Gateway = hass.data[DOMAIN][config_entry.entry_id]
     aqara_gateway.add_setup('switch', setup)
-
-
-async def async_unload_entry(hass, entry):
-    # pylint: disable=unused-argument
-    """ unload entry """
-    return True
 
 
 class GatewaySwitch(GatewayGenericDevice, SwitchEntity, RestoreEntity):
     """Representation of a Xiaomi/Aqara Plug."""
 
-    def __init__(self, gateway: Gateway, device: dict, attr: str, feature):
-        """Initialize the XiaomiPlug."""
-        self._chip_temperature = None
-        self._fw_ver = None
-        self._in_use = None
-        self._load_power = None
-        self._lqi = None
-        self._power_consumed = None
-        self._voltage = None
-        self._model = device['model']
-        self.feature = feature
-        super().__init__(gateway, device, attr)
+    _attr_icon: str | None = "mdi:power-socket"
 
-    async def async_added_to_hass(self):
-        """Run when entity about to be added."""
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added to hass."""
         if last_state := await self.async_get_last_state():
             if last_state.state == STATE_ON:
                 self._attr_is_on = True
@@ -67,59 +38,16 @@ class GatewaySwitch(GatewayGenericDevice, SwitchEntity, RestoreEntity):
                 self._attr_is_on = False
         await super().async_added_to_hass()
 
-    @property
-    def icon(self):
-        """return icon."""
-        return 'mdi:power-socket'
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes."""
-        self._attrs[ATTR_CHIP_TEMPERATURE] = self._chip_temperature
-        self._attrs[ATTR_FW_VER] = self._fw_ver
-        self._attrs[ATTR_LQI] = self._lqi
-        if self.feature.get('support_power_consumption', False):
-            self._attrs[ATTR_LOAD_POWER] = self._load_power
-            self._attrs[ATTR_POWER_CONSUMED] = self._power_consumed
-
-        if self.feature.get('support_in_use', False):
-            self._attrs[ATTR_IN_USE] = self._in_use
-        if self.feature.get('support_load_voltage', False):
-            self._attrs[ATTR_VOLTAGE] = self._voltage
-        return self._attrs
-
-    def update(self, data: dict):
+    def update(self, data: dict) -> None:
         """update switch."""
-        for key, value in data.items():
-            if key == CHIP_TEMPERATURE:
-                self._chip_temperature = value
-            if key == FW_VER or key == 'back_version':
-                self._fw_ver = value
-            if key == LOAD_POWER:
-                self._load_power = value
-            if key == LQI:
-                self._lqi = value
-            if key == ENERGY_CONSUMED:
-                self._power_consumed = value
-            if key == IN_USE:
-                self._in_use = bool(value)
-            if key == LOAD_VOLTAGE:
-                self._voltage = format(
-                    float(value) / 1000, '.3f') if isinstance(
-                    value, (int, float)) else None
-            if key in SWITCH_ATTRIBUTES:
-                self._attrs[key] = value
-            if key == self._attr:
-                if self._model in ["aqara.feeder.acn001"] and self._attr == "feed_switch":
-                    self._attr_is_on = False
-                else:
-                    self._attr_is_on = bool(value)
-        self.async_write_ha_state()
+        if self._attr in data:
+            self._attr_is_on = bool(data[self._attr])
+            self.async_write_ha_state()
 
-    def turn_on(self, **kwargs):
-        """Turn the switch on."""
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the entity on."""
         self.gateway.send(self.device, {self._attr: 1})
 
-    def turn_off(self, **kwargs):
-        """Turn the switch off."""
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the entity off."""
         self.gateway.send(self.device, {self._attr: 0})
